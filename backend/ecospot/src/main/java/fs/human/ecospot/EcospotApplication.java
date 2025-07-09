b6e0e3c3ef5055428865d2ffcbbe59e4b6564ad1
yyk <yyk0915@nate.com>
2025-07-09 12:15:20 +0900

    new file:   .idea/.gitignore 	new file:   .idea/No1ccb.iml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   backend/.idea/.gitignore 	new file:   backend/.idea/backend.iml 	new file:   backend/.idea/gradle.xml 	new file:   backend/.idea/misc.xml 	new file:   backend/.idea/modules.xml 	new file:   backend/.idea/vcs.xml 	new file:   backend/ecospot/.gitattributes 	new file:   backend/ecospot/.gitignore 	new file:   backend/ecospot/build.gradle 	new file:   backend/ecospot/gradle/wrapper/gradle-wrapper.jar 	new file:   backend/ecospot/gradle/wrapper/gradle-wrapper.properties 	new file:   backend/ecospot/gradlew 	new file:   backend/ecospot/gradlew.bat 	new file:   backend/ecospot/settings.gradle 	new file:   backend/ecospot/src/main/java/fs/human/ecospot/EcospotApplication.java 	new file:   backend/ecospot/src/main/java/fs/human/ecospot/HelloWorldController.java 	new file:   backend/ecospot/src/main/resources/application.properties 	new file:   backend/ecospot/src/test/java/fs/human/ecospot/EcospotApplicationTests.java 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/favicon.ico 	new file:   frontend/public/index.html 	new file:   frontend/public/logo192.png 	new file:   frontend/public/logo512.png 	new file:   frontend/public/manifest.json 	new file:   frontend/public/robots.txt 	new file:   frontend/src/App.css 	new file:   frontend/src/App.js 	new file:   frontend/src/App.test.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js 	new file:   frontend/src/logo.svg 	new file:   frontend/src/reportWebVitals.js 	new file:   frontend/src/setupTests.js

---
+++ backend/ecospot/src/main/java/fs/human/ecospot/EcospotApplication.java
@@ -0,0 +1,13 @@
+package fs.human.ecospot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcospotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcospotApplication.class, args);
+	}
+
+}
---
+++ backend/ecospot/src/test/java/fs/human/ecospot/EcospotApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.human.ecospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcospotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
